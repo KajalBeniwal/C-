ff0f01aa819a8adc3159badd9d8cc83c7d5ba758
KajalBeniwal <beniwalk662@gmail.com>
2025-11-12 18:59:32 +0530

    Update student names in Program5

--- Program5.cs
+++ Program5.cs
@@ -38,11 +38,12 @@ class Program5
     static void Main()
     {
         Student s1 = new Student();
-        Student s2 = new Student("Rahul", 101);
-        Student s3 = new Student("Amit", 102, 88.5);
+        Student s2 = new Student("Sahil", 101);
+        Student s3 = new Student("Sumit", 102, 88.5);
 
         s1.Display();
         s2.Display();
         s3.Display();
     }
 }
+
